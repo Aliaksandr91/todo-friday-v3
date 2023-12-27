d26346bd45bdec0b2cf7b5921d56a4dc72e50c89
Aliaksandr91 <horbachalex91@gmail.com>
2023-12-27 20:26:25 +0100

    feat: login logout initialize thunks reffactoring

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -3,9 +3,9 @@ import { useFormik } from "formik";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
-import { loginTC } from "features/auth/auth.reducer";
 import { useAppDispatch } from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/auth.selectors";
+import {authThunks} from "features/auth/auth.reducer";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
@@ -31,7 +31,7 @@ export const Login = () => {
       rememberMe: false,
     },
     onSubmit: (values) => {
-      dispatch(loginTC(values));
+      dispatch(authThunks.login(values));
     },
   });
 
