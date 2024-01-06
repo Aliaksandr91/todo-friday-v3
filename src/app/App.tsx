461d8c04da9f2b04b6a1ea62c42fc5db2a6473bd
Aliaksandr91 <horbachalex91@gmail.com>
2024-01-06 17:56:34 +0100

    feat: TodolisList upd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ import { Login } from "features/auth/Login";
 import "./App.css";
 import { TodolistsList } from "features/TodolistsList/TodolistsList";
 import { ErrorSnackbar } from "common/components";
-import { useAppDispatch } from "common/hooks";
+import {useActions, useAppDispatch} from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/auth.selectors";
 import { selectAppStatus, selectIsInitialized } from "app/app.selectors";
 import {authThunks} from "features/auth/auth.reducer";
@@ -27,14 +27,14 @@ function App() {
   const isInitialized = useSelector(selectIsInitialized);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const dispatch = useAppDispatch();
 
+  const {initializeApp, logout} = useActions(authThunks)
   useEffect(() => {
-    dispatch(authThunks.initializeApp());
+    initializeApp()
   }, []);
 
   const logoutHandler = useCallback(() => {
-    dispatch(authThunks.logout());
+    logout()
   }, []);
 
   if (!isInitialized) {
